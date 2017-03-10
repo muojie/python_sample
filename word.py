522fb77034ad2ed44b5068d3c66e731f0822eb37
guoxinjie <guoxinjie@haima.me>
2017-03-10 15:27:20 +0800

    code style

--- word.py
+++ word.py
@@ -15,9 +15,10 @@ def un_zip(file_name, tag_dir):
         zip_file.extract(names,tag_dir)  
     zip_file.close()  
 
-def extractAndFormat(inFileName, outFileName, keyword):
-    if(os.path.exists(inFileName)):
-        with open(inFileName, 'r', encoding="utf8") as infile, open(outFileName, 'w') as outfile:
+
+def extractAndFormat(in_filename, out_filename, keyword):
+    if(os.path.exists(in_filename)):
+        with open(in_filename, 'r', encoding="utf8") as infile, open(out_filename, 'w') as outfile:
             copy = False
             for line in infile:
                 if line.find(keyword) == -1:
@@ -31,11 +32,11 @@ def extractAndFormat(inFileName, outFileName, keyword):
                     outfile.write(str2)
             outfile.close()
     else:
-       print(inFileName + " ========> no this file")
+       print(in_filename + " ========> no this file")
 
 
-def log2csv(filename, csvFileName):
-    with open(filename, 'r', encoding="utf8") as infile, open(csvFileName, 'w', encoding='utf8',newline='') as csvfile:
+def log2csv(filename, csv_filename):
+    with open(filename, 'r', encoding="utf8") as infile, open(csv_filename, 'w', encoding='utf8',newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
         head = ['describe', 'ts_got', 'ts_decoded', 'ts_torende']
         writer.writerow(head)
@@ -44,37 +45,43 @@ def log2csv(filename, csvFileName):
                 writer.writerow(line)
         csvfile.close()
 
+
 def formatcsv(filename):
-    csvFilename = filename
-    with open(csvFilename, 'r+', encoding='utf8',newline='') as csvfile:
+    csv_filename = filename
+    with open(csv_filename, 'r+', encoding='utf8',newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
         reader = csv.reader(csvfile, delimiter=",")
         head = ['describe', 'ts_got', 'ts_decoded', 'ts_torende', "decode_time", "to_render_time", "total_time", "speed_test_time", "speed_test_value", "op_start", "op_10", "op_5", "first_frame", "mediaCodec"]
-        #writer.writerow(head)
+        # writer.writerow(head)
         csvfile.close()
 
-def formatcsv2(csvFileNameGot, csvFileNameQueue, csvFileNameVideo, csvFileNameAll):
-    f = open(csvFileNameAll, 'w+')
+
+def formatcsv2(csv_filename_got, csv_filename_queue, csv_filename_video, csv_filename_all):
+    f = open(csv_filename_all, 'w+')
     print("ts_pts, ts_got, ts_queue, ts_dequeue, ts_render, to_decode, decode_time, to_render_time, total_time", file=f)  
-    for line in open(csvFileNameGot, 'r', encoding='utf8',newline=''):
+    for line in open(csv_filename_got, 'r', encoding='utf8',newline=''):
         line=line.strip('\n')
         line=line.strip('\r')
         str_got, ts_pts, ts_got = line.split(",")
-        for line in open(csvFileNameQueue, 'r', encoding='utf8',newline=''):
+        ts_list = [ts_pts, ts_got]
+        for line in open(csv_filename_queue, 'r', encoding='utf8',newline=''):
             line=line.strip('\n')
             line=line.strip('\r')
             str_queue, ts_pts_queue, ts_queue = line.split(",")
-            if(ts_pts == ts_pts_queue):
-                for line in open(csvFileNameVideo, 'r', encoding='utf8',newline=''):
+            if ts_pts == ts_pts_queue:
+                ts_list.append(ts_queue)
+                for line in open(csv_filename_video, 'r', encoding='utf8',newline=''):
                     line=line.strip('\n')
                     line=line.strip('\r')
                     str_video, ts_queue_video, ts_dequeue, ts_render = line.split(",")
-                    #print(ts_queue_video, ts_dequeue, ts_render)
-                    #print(type(ts_queue_video), type(ts_queue))
-                    #print(len(ts_queue_video), len(ts_queue))
-                    if ts_queue == ts_queue_video :
-                        #print(ts_pts, ts_got, ts_pts_queue, ts_queue, ts_queue_video, ts_dequeue, ts_render)
-                        #print(ts_pts, ts_queue, ts_dequeue, ts_render)
+                    # print(ts_queue_video, ts_dequeue, ts_render)
+                    # print(type(ts_queue_video), type(ts_queue))
+                    # print(len(ts_queue_video), len(ts_queue))
+                    if ts_queue == ts_queue_video:
+                        ts_list.append(ts_dequeue)
+                        ts_list.append(ts_render)
+                        # print(ts_pts, ts_got, ts_pts_queue, ts_queue, ts_queue_video, ts_dequeue, ts_render)
+                        # print(ts_pts, ts_queue, ts_dequeue, ts_render)
                         to_decode = int(ts_queue) - int(ts_got)
                         decode_time = int(ts_dequeue) - int(ts_queue)
                         to_render = int(ts_render) - int(ts_dequeue)
@@ -82,36 +89,38 @@ def formatcsv2(csvFileNameGot, csvFileNameQueue, csvFileNameVideo, csvFileNameAl
                         print("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}".format(ts_pts, ts_got, ts_queue, ts_dequeue, ts_render, to_decode, decode_time, to_render, total_time), file=f)  
                         break
                 break
+        if len(ts_list) < 5:
+            print(ts_list)
 
     
-#    with open(csvFileNameGot, 'r+', encoding='utf8',newline='') as csvfile_got, open(csvFileNameAll, 'w', encoding='utf8',newline='') as csvfile_all:
-#        writer = csv.writer(csvfile_all, delimiter=",")
-#        reader = csv.reader(csvfile_got, delimiter=",")
-#        head = ['describe', 'ts_pts', 'ts_got', 'ts_decoded', 'ts_torende', 'decode_time', 'to_render_time', 'total_time']
-#        writer.writerow(head)
+   # with open(csv_filename_got, 'r+', encoding='utf8',newline='') as csvfile_got, open(csv_filename_all, 'w', encoding='utf8',newline='') as csvfile_all:
+   #     writer = csv.writer(csvfile_all, delimiter=",")
+   #     reader = csv.reader(csvfile_got, delimiter=",")
+   #     head = ['describe', 'ts_pts', 'ts_got', 'ts_decoded', 'ts_torende', 'decode_time', 'to_render_time', 'total_time']
+   #     writer.writerow(head)
         
-def log_from_tecent(rootDir):
-    unzipDir =  rootDir + "/log_from_zip/"
-    resultDir = rootDir + "/log_result/"
+def log_from_tecent(root_dir):
+    unzip_dir =  root_dir + "/log_from_zip/"
+    result_dir = root_dir + "/log_result/"
     
-    if os.path.isdir(unzipDir):
-        print("deleting dir "+unzipDir)
-        shutil.rmtree(unzipDir)
+    if os.path.isdir(unzip_dir):
+        print("deleting dir "+unzip_dir)
+        shutil.rmtree(unzip_dir)
         print("done")
 
-    if os.path.isdir(resultDir):
-        print("deleting dir "+resultDir)
-        shutil.rmtree(resultDir)
+    if os.path.isdir(result_dir):
+        print("deleting dir "+result_dir)
+        shutil.rmtree(result_dir)
         print("done")
     
-    os.mkdir(unzipDir)
-    os.mkdir(resultDir)
+    os.mkdir(unzip_dir)
+    os.mkdir(result_dir)
     
-    for lists in os.listdir(rootDir): 
-        path = os.path.join(rootDir, lists)
+    for lists in os.listdir(root_dir):
+        path = os.path.join(root_dir, lists)
         if os.path.isdir(path):
             print(path)
-            tag_dir = os.path.join(unzipDir, lists)
+            tag_dir = os.path.join(unzip_dir, lists)
 
             if os.path.isdir(tag_dir):  
                 pass  
@@ -123,59 +132,59 @@ def log_from_tecent(rootDir):
                 if zipfile.is_zipfile(filename):
                     un_zip(filename, tag_dir)
                 
-            inFileName = os.path.join(tag_dir, 'log.txt')
-#            outFileName = os.path.join(resultDir, os.path.basename(tag_dir)+'.log.output')
-            csvFileName = os.path.join(resultDir, os.path.basename(tag_dir)+'.csv')
+            in_filename = os.path.join(tag_dir, 'log.txt')
+            # out_filename = os.path.join(result_dir, os.path.basename(tag_dir)+'.log.output')
+            csv_filename = os.path.join(result_dir, os.path.basename(tag_dir)+'.csv')
             keyword = "CloudTest>>"
-            extractAndFormat(inFileName, csvFileName, keyword)
-            if(os.path.exists(csvFileName)):
-                formatcsv(csvFileName)
-#                log2csv(outFileName, csvFileName)
-                
-def log_from_mine(rootDir):
+            extractAndFormat(in_filename, csv_filename, keyword)
+            if os.path.exists(csv_filename):
+                formatcsv(csv_filename)
+                # log2csv(out_filename, csv_filename)
 
-    unzipDir =  rootDir + "/log_from_zip/"
-    resultDir = rootDir + "/log_result/"
+
+def log_from_mine(root_dir):
+
+    unzip_dir = root_dir + "/log_from_zip/"
+    result_dir = root_dir + "/log_result/"
     
-    if os.path.isdir(resultDir):
-        print("deleting dir "+resultDir)
-        shutil.rmtree(resultDir)
+    if os.path.isdir(result_dir):
+        print("deleting dir "+result_dir)
+        shutil.rmtree(result_dir)
         print("done")
 
-    os.mkdir(resultDir)
+    os.mkdir(result_dir)
     
-    for lists in os.listdir(rootDir): 
-        path = os.path.join(rootDir, lists)
+    for lists in os.listdir(root_dir):
+        path = os.path.join(root_dir, lists)
         if os.path.isdir(path):
             print(path)
-            tag_dir = os.path.join(unzipDir, lists)
+            tag_dir = os.path.join(unzip_dir, lists)
 
-            inFileName = os.path.join(tag_dir, 'log.txt')
-            #outFileName = os.path.join(resultDir, os.path.basename(tag_dir)+'.log.output')
+            in_filename = os.path.join(tag_dir, 'log.txt')
+            # out_filename = os.path.join(result_dir, os.path.basename(tag_dir)+'.log.output')
 
-            csvFileNameGot = os.path.join(resultDir, os.path.basename(tag_dir)+'.got.csv')
+            csv_filename_got = os.path.join(result_dir, os.path.basename(tag_dir)+'.got.csv')
             keyword = "frame_process(got)"
-            extractAndFormat(inFileName, csvFileNameGot, keyword)
+            extractAndFormat(in_filename, csv_filename_got, keyword)
             
-            csvFileNameQueue = os.path.join(resultDir, os.path.basename(tag_dir)+'.queue.csv')
+            csv_filename_queue = os.path.join(result_dir, os.path.basename(tag_dir)+'.queue.csv')
             keyword = "frame_process(queue)"
-            extractAndFormat(inFileName, csvFileNameQueue, keyword)
+            extractAndFormat(in_filename, csv_filename_queue, keyword)
 
-            csvFileNameVideo = os.path.join(resultDir, os.path.basename(tag_dir)+'.video.csv')
+            csv_filename_video = os.path.join(result_dir, os.path.basename(tag_dir)+'.video.csv')
             keyword = "frame_process(video)"
-            extractAndFormat(inFileName, csvFileNameVideo, keyword)
+            extractAndFormat(in_filename, csv_filename_video, keyword)
 
-            csvFileNameAll = os.path.join(resultDir, os.path.basename(tag_dir)+'.all.csv')
+            csv_filename_all = os.path.join(result_dir, os.path.basename(tag_dir)+'.all.csv')
 
-            if(os.path.exists(csvFileNameGot)):
-                formatcsv2(csvFileNameGot, csvFileNameQueue, csvFileNameVideo, csvFileNameAll)
-                #log2csv(outFileName, csvFileName)
+            if os.path.exists(csv_filename_got):
+                formatcsv2(csv_filename_got, csv_filename_queue, csv_filename_video, csv_filename_all)
+                # log2csv(out_filename, csv_filename)
 
-tecentDir =r'C:\Users\lenovo\Desktop\tx_round_1'
-myDir =r'C:\Users\lenovo\Desktop\cloudtest\vivo\release'
+tecentDir = r'C:\Users\lenovo\Desktop\tx_round_1'
+myDir = r'C:\Users\lenovo\Desktop\cloudtest\nubia\release'
 
 
-
-#log_from_tecent(tecnetDir)
+# log_from_tecent(tecnetDir)
 log_from_mine(myDir)
 
